0d9c4b039022fa1bd3e1540e0e643069a58801a5
Anna Tao <148929819+atao2004@users.noreply.github.com>
2026-04-30 16:45:27 -0700

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.py
@@ -0,0 +1,39 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # n = len(nums)
+        # memo = [0] * (len(nums) + 1)
+        # memo[0] = nums[0]
+        # if n > 1:
+        #     memo[1] = max(nums[0], nums[1])
+        # for i in range(2, n):
+        #     memo[i] = max(memo[i-1], nums[i] + memo[i-2])
+        # return memo[n-1]
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        a=nums[0]
+        b = max(nums[1], nums[0])
+        for i in range(2, n):
+            a,b = b, max(b, nums[i] + a)
+        return max(a, b)
+
+
+
+
+
+
+        # def dfs(i):
+        #     if i >=n:
+        #         return memo[n]
+        #     elif memo[i] != 0:
+        #         return memo[i]
+        #     memo[i] = max(dfs(i+1), nums[i] + dfs(i+2))
+        #     return memo[i]
+        # return dfs(0)
+            
+        # def dfs(i):
+        #     if i >= n:
+        #         return 0
+        #     return max(dfs(i+1), nums[i] + dfs(i + 2))
+        # return dfs(0)
+        
