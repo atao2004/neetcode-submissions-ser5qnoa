28402a03d3379cd3060261f053f2c2c8ec06a4a1
Anna Tao <148929819+atao2004@users.noreply.github.com>
2026-04-30 11:57:35 -0700

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # n = 3 => (n=2)+(n=1)+(n=0)
+        # memo arr which stores index:num steps
+        memo = [0] * (n+1)
+        memo[0] = 0
+        memo[1] = 1
+        if n >= 2:
+            memo[2] = 2
+            for i in range(3, n+1):
+                memo[i] = memo[i-1] + memo[i-2]
+        return memo[n]
+        # def dp(i):
+        #     if i == 1:
+        #         return 1
+        #     elif i == 2:
+        #         return 2
+        #     elif memo[i] != 0:
+        #         return memo[i]
+        #     memo[i] = dp(i-1) + dp(i-2)
+        #     return memo[i]
+        # return dp(n)
+
